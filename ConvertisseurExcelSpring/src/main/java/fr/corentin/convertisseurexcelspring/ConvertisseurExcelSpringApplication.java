0a0f6c9bee9dc254545389001450bb4134337b7c
HarvoireC <corentin.harvoire@gmail.com>
2026-01-14 10:10:59 +0100

    init

---
+++ ConvertisseurExcelSpring/src/main/java/fr/corentin/convertisseurexcelspring/ConvertisseurExcelSpringApplication.java
@@ -0,0 +1,13 @@
+package fr.corentin.convertisseurexcelspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvertisseurExcelSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvertisseurExcelSpringApplication.class, args);
+	}
+
+}
---
+++ ConvertisseurExcelSpring/src/test/java/fr/corentin/convertisseurexcelspring/ConvertisseurExcelSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.corentin.convertisseurexcelspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertisseurExcelSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
